a92f8dc627553346157cd56bd834a5962287e735
Xu <3176740@csmv.qc.ca>
2026-01-13 12:49:41 -0500

    removed dummyposts

--- apps/backend/pyred/urls.py
+++ apps/backend/pyred/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pyreddit.urls')),
+    
 
 ]
 
--- apps/backend/pyreddit/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import DummyPosts
-
-urlpatterns = [
-    path('api/posts/', DummyPosts.as_view(), name='dummy-posts'),
-]
