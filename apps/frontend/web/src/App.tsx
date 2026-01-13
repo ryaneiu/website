b129e00bde739625a91cb955bda2de0b07665035
= <publicuser0999847695359@gmail.com>
2026-01-12 19:39:49 -0500

    create react project

---
+++ apps/frontend/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return <div className='flex w-100 h-100 items-center justify-center'>
+    <h1>This will actually be a good looking UI eventually, trust me bro,</h1>
+  </div>
+}
+
+export default App
