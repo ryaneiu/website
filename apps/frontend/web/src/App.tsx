0d388401f469b68f11d3e551c0ee56bdffefadfa
= <publicuser0999847695359@gmail.com>
2026-01-12 19:45:42 -0500

    fix

--- apps/frontend/web/src/App.tsx
+++ apps/frontend/web/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
 
-  return <div className='flex w-100 h-100 items-center justify-center'>
-    <h1>This will actually be a good looking UI eventually, trust me bro,</h1>
+  return <div className='flex w-[100vw] h-[100vh] items-center justify-center'>
+    <h1 className="font-bold text-3xl">This will actually be a good looking UI eventually, trust me bro</h1>
   </div>
 }
 
