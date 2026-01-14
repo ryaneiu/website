84ab5ac2376721af7ba5a990ef4d30ef99573261
= <publicuser0999847695359@gmail.com>
2026-01-13 19:42:24 -0500

    rename

--- apps/frontend/web/src/navbar/Navbar.tsx
+++ apps/frontend/web/src/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "../components/Button";
 
 export function Navbar() {
     return <div className="gap-4 w-[100vw] h-16 bg-white border-b border-b-black/15 flex items-center px-2 py-2">
-        <h1 className="font-bold text-2xl">Definitly Not Reddit Clone</h1>
+        <h1 className="font-bold text-2xl">React-2-Pytailred</h1>
         
         <input className="flex-grow-1 rounded-md border border-black/15 px-2 py-2" placeholder="Giant Search Bar... Search?"></input>
 
