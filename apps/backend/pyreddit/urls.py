7383966b833498db75b7ad43dd4b7b6c0ac6970f
ryan <ryan.mh.xu@gmail.com>
2026-01-16 18:06:55 -0500

    added stuff

--- apps/backend/pyreddit/urls.py
+++ apps/backend/pyreddit/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import signup, protected
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
@@ -14,10 +13,7 @@ urlpatterns = [
 
 
 
-    path('signup/', signup),
-    path('login/', TokenObtainPairView.as_view()),   # token login
-    path('refresh/', TokenRefreshView.as_view()),    # token refresh
-    path('protected/', protected),
+    
 
     path('', include(router.urls)),  # router handles /posts/ and /comments/
 ]
