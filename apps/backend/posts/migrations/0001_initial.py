4837ac8432f03a402a25692d8e98a9f8b18d1dfb
= <publicuser0999847695359@gmail.com>
2026-02-12 20:59:43 -0500

    remove trailing whitespaces

--- apps/backend/posts/migrations/0001_initial.py
+++ apps/backend/posts/migrations/0001_initial.py
@@ -24,17 +24,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, 
-                                           primary_key=True, 
-                                           serialize=False, 
+                ('id', models.BigAutoField(auto_created=True,
+                                           primary_key=True,
+                                           serialize=False,
                                            verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),
                 ('content', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('published', models.BooleanField(default=False)),
                 ('author', models.ForeignKey(
-                    on_delete=django.db.models.deletion.CASCADE, 
-                    related_name='posts_posts', 
+                    on_delete=django.db.models.deletion.CASCADE,
+                    related_name='posts_posts',
                     to=settings.AUTH_USER_MODEL
                 )),
             ],
