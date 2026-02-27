df1a0f5c1d1d79eb6bf3ff5cd98a12e2f67fb619
ryan <ryan.mh.xu@gmail.com>
2026-02-27 14:29:22 -0500

    lets NOT go because the posts at backend doesnt work

--- apps/backend/pyred/templates/dist/assets/Trending-H7GKGbXl.js
+++
@@ -1,1 +0,0 @@
-import{j as e,k as n}from"./index-Dxn2WRx0.js";function i(){return e.jsx(n,{children:e.jsx("h1",{children:"TODO: Trending page"})})}export{i as default};
---
+++ apps/backend/pyred/templates/dist/assets/Trending-KLY74yWD.js
@@ -0,0 +1,1 @@
+import{j as e,k as n}from"./index-DB3mfglz.js";function i(){return e.jsx(n,{children:e.jsx("h1",{children:"TODO: Trending page"})})}export{i as default};
