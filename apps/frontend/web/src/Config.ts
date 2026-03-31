2713ef8d53380836352b49f2eb55cf0d1e75622f
ryan <ryan.mh.xu@gmail.com>
2026-03-31 19:56:52 -0400

    gonna implement dark mode next

--- apps/frontend/web/src/Config.ts
+++ apps/frontend/web/src/Config.ts
@@ -5,6 +5,6 @@ const hostname = window.location.hostname;
 const port = window.location.port;
 const isViteDevServer =
     (hostname === "localhost" || hostname === "127.0.0.1") &&
-    (port === "5173" || port === "4173");
+    (port === "5173" || port === "5174" || port === "4173");
 
 export const API_ENDPOINT = isViteDevServer ? "http://127.0.0.1:8001" : "";
