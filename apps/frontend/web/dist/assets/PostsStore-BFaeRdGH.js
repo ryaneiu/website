0cd5106aa4e41843345449b4c7292648bd71c804
= <publicuser0999847695359@gmail.com>
2026-04-18 18:25:26 -0400

    ungitignore dist because we need it for backend

---
+++ apps/frontend/web/dist/assets/PostsStore-BFaeRdGH.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-B1WS3xKF.js";const t=s(()=>({posts:[],hasLoaded:!1}));export{t as p};
---
+++ apps/frontend/web/dist/assets/Trending-DPuCgedg.js
@@ -0,0 +1,1 @@
+import{j as n,p as e}from"./index-B1WS3xKF.js";function i(){return n.jsx(e,{children:n.jsx("main",{children:n.jsx("h1",{children:"TODO: Trending page"})})})}export{i as default};
